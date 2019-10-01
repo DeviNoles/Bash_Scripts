adea352b046b03b8a473ff5b61d57a868a26f81c
DevinM6 <devin8821@gmail.com>
2019-09-30 20:19:27 -0400

    Updated maxfile

--- maxfile.sh
+++ maxfile.sh
@@ -1,31 +1,32 @@
 #/bin/bash
-rt=$PWD
+rt=$1
 max_size=0
+cd $1
+rt=$PWD
 function mf
 {
-count=0
 for dir in $(ls -A)
 do
+    echo "$dir" in $PWD
     if [[ -d $dir ]]; then
      echo "$dir is a DIRECTORY"
      cd $dir
-     count=$(($count+1))
      mf
     elif [[ -f $dir ]]; then
-     echo "$dir is a FILE"
+     #echo "$dir is a FILE"
      current_size=`du -k "$dir" | cut -f1`
      if (($current_size>$max_size)); then
       dr=$PWD
       fl=$dir
       max_size=$current_size
-    fi
-   else
-     echo $dir
-     cd $rt
-
+     fi
+   elif [[ -L "$file" && -d "$file" ]]; then
+    # echo "$dir is a SYMLINK"
+     cd $dir
+     mf
 fi
-
+cd $rt
 done
 }
 mf
-echo Largest file is: $fl with a size of $max_size Kb located in $dr
+echo Largest file: $fl with a size of $max_size Kb located in $dr
